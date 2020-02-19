79850875a4c1f18c90e51b58f49e2e7d3039db6c
retroheim <60027238+retroheim@users.noreply.github.com>
2020-02-18 19:43:52 -0500

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,7 @@ if [ "$2" == "--setup" ] || [ "$3" == "--setup" ] || [ "$4" == "--setup" ]; then
 	echo "[Akarin] Setup Paper.."
 	(
 		if [ "$2" == "--remote" ] || [ "$3" == "--remote" ] || [ "$4" == "--remote" ]; then
+			git submodule update --init --recursive
 			cd "$paperworkdir/Paper-Server/src/main/java"
 			if [ -d "Minecraft" ]; then
 				rm Minecraft/ -r
